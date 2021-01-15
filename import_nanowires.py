0236e5029c542dc83074ba606965ecc401d1cdc4
Jesus Lares <jelares@jesuss-mbp.attlocal.net>
2021-01-15 16:34:42 -0500

    first commit

---
+++ import_nanowires.py
@@ -0,0 +1,9 @@
+import PySpice.Spice.Library as splib
+
+nanowire_lib = splib.SpiceLibrary("./libraryTest")
+
+nanowireBCF = nanowire_lib['nanowireBCF']
+# nanowireDynamic = nanowire_lib['nanowireDynamic']
+
+# print(nanowireBCF)
+# print(nanowireDynamic)
